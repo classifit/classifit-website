9907c1443d61b42399838aba231ed3db559f0007
Chiara Santi <santichiara@hotmail.com>
2023-10-25 11:31:04 +0200

    variables and buttons

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,6 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <Button>test</Button>
+    <Button variant="ghost">Test</Button>
   )
 }
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -5,19 +5,19 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-lg text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
         default:
-          "bg-primary text-primary-foreground shadow hover:bg-primary/90",
+          "bg-primary text-primary-foreground hover:bg-primary-hover focus-visible:ring-ring",
         destructive:
-          "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
+          "bg-destructive text-destructive-foreground hover:bg-destructive-hover focus-visible:ring-destructivering",
         outline:
-          "border border-input bg-transparent shadow-sm hover:bg-accent hover:text-accent-foreground",
+          "border border-input bg-transparent hover:bg-accent hover:text-accent-foreground focus-visible:ring-ring",
         secondary:
-          "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
+          "bg-secondary text-secondary-foreground hover:bg-secondary-hover focus-visible:ring-ring",
+        ghost: "hover:bg-accent hover:text-accent-foreground focus-visible:ring-ring",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
