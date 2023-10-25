b8ce12423889782011b032527cef9a280a3d8fcd
Chiara Santi <santichiara@hotmail.com>
2023-10-25 19:25:35 +0200

    hero

--- components/sections/hero.tsx
+++ components/sections/hero.tsx
@@ -1,15 +1,21 @@
+import { ArrowRightIcon } from "@radix-ui/react-icons";
 import { Button } from "../ui/button";
 
 export default function Hero() {
-    return(
-        <div className="flex items-center justify-center max-w-[1280px] px-12 py-36 mx-auto bg-primary text-primary-foreground text-center">
-            <div className="flex flex-col gap-20 w-3/4">
-                <div className="flex flex-col gap-8">
-                    <h1 className="font-semibold text-7xl">Porta il tuo studio al prossimo livello</h1>
-                    <p className="opacity-60 w-3/4 mx-auto">Gestisci lezioni, pacchetti, clienti e potenzia la tua identità e la tua presenza online - Tutto con la stessa piattaforma</p>
-                </div>
-                <Button variant="secondary">Contattaci - Senza impegno</Button>
-            </div>
-        </div>
-    )
+  return (
+    <div className="max-w-[1280px] px-12 py-36 mx-auto bg-primary text-primary-foreground">
+      <div className="w-3/4">
+        <h1 className="text-7xl font-semibold">
+          Porta il tuo studio al prossimo livello
+        </h1>
+        <div className="py-4" />
+        <p className="w-2/3">
+          Gestisci lezioni, pacchetti, clienti e potenzia la tua identità e la
+          tua presenza online - Tutto con la stessa piattaforma
+        </p>
+        <div className="py-10" />
+        <Button variant="secondary">Contattaci - Senza impegno<ArrowRightIcon className="ml-3 h-4 w-4" /></Button>
+      </div>
+    </div>
+  );
 }
