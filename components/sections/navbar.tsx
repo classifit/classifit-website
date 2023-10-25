e46052076a7a32c30f18ba2ee4911d5112dc605c
Chiara Santi <santichiara@hotmail.com>
2023-10-25 15:25:37 +0200

    navbar layout

--- components/sections/navbar.tsx
+++ components/sections/navbar.tsx
@@ -1,13 +1,67 @@
 import Image from "next/image";
 import MenuItem from "../ui/menu-item";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+  CalendarIcon,
+  DesktopIcon,
+  TimerIcon,
+  ArchiveIcon,
+  StarIcon,
+  ArrowRightIcon
+} from "@radix-ui/react-icons";
+import { Button } from "../ui/button";
 
 export default function Navbar() {
-    return(
-        <div className="flex items-center justify-between max-w-[1280px] px-12 py-4 bg-primary">
-            <div className="flex items-center gap-12">
-                <Image src="/classifit.svg" width="101" height="24" alt="Logo orizzontale di Classifit" priority/>
-                <MenuItem text="Prova" link="#"/>
-            </div>
-        </div>
-    )
+  return (
+    <nav className="flex items-center justify-between max-w-[1280px] px-12 py-6 bg-primary mx-auto">
+      <div className="flex items-center gap-12">
+        <Image
+          src="/classifit.svg"
+          width="101"
+          height="24"
+          alt="Logo orizzontale di Classifit"
+          priority
+        />
+        <DropdownMenu>
+          <DropdownMenuTrigger className="opacity-60 hover:opacity-100 text-sm">
+            Servizi
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="mt-1">
+            <DropdownMenuItem className="group">
+              <TimerIcon className="mr-3 h-4 w-4 text-muted-foreground group-hover:text-accent-foreground" />
+              Prenotazioni
+            </DropdownMenuItem>
+            <DropdownMenuItem className="group">
+              <CalendarIcon className="mr-3 h-4 w-4 text-muted-foreground group-hover:text-accent-foreground" />
+              Palinsesto
+            </DropdownMenuItem>
+            <DropdownMenuItem className="group">
+              <ArchiveIcon className="mr-3 h-4 w-4 text-muted-foreground group-hover:text-accent-foreground" />
+              Pacchetti a crediti
+            </DropdownMenuItem>
+            <DropdownMenuItem className="group">
+              <DesktopIcon className="mr-3 h-4 w-4 text-muted-foreground group-hover:text-accent-foreground" />
+              Sito su misura
+            </DropdownMenuItem>
+            <DropdownMenuItem className="group">
+              <StarIcon className="mr-3 h-4 w-4 text-muted-foreground group-hover:text-accent-foreground" />
+              Servizi aggiuntivi
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+        <MenuItem text="Studi" link="#" />
+        <MenuItem text="Aiuto" link="#" />
+        <MenuItem text="@app.classi.fit" link="#" />
+      </div>
+      <div className="flex items-center gap-12">
+        <MenuItem text="Accedi" link="#" />
+        <Button variant="secondary">Contattaci<ArrowRightIcon className="ml-3 h-4 w-4" /></Button>
+      </div>
+    </nav>
+  );
 }
--- components/ui/menu-item.tsx
+++ components/ui/menu-item.tsx
@@ -7,6 +7,6 @@ type Props = {
 
 export default function MenuItem(props: Props) {
     return(
-        <Link href={props.link} className="text-primary-foreground opacity-60 hover:opacity-100 text-sm transition-opacity ease-in-out duration-200">{props.text}</Link>
+        <Link href={props.link} className="select-none text-primary-foreground opacity-60 hover:opacity-100 text-sm transition-opacity ease-in-out duration-200">{props.text}</Link>
     )
 }
