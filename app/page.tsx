3ae3f95e70c5f7dde440a075ba70bc19de594f64
Chiara Santi <santichiara@hotmail.com>
2023-10-27 09:25:28 +0200

    added contact button globally

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,11 @@ import Hero from '@/components/sections/hero'
 import Navbar from '@/components/sections/navbar'
 import Packages from '@/components/sections/packages'
 import Portfolio from '@/components/sections/portfolio'
-import ContactButton from '@/components/ui/contact-button'
 
 export default function Home() {
   return (
     <>
     <Navbar />
-    <ContactButton />
     <Hero />
     <Feature />
     <Packages />
