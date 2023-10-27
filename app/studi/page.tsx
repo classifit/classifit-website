755097659d4004aa0075ed511f614d87b259de9a
Chiara Santi <santichiara@hotmail.com>
2023-10-27 09:58:36 +0200

    studios and double images component

--- app/studi/page.tsx
+++ app/studi/page.tsx
@@ -1,7 +1,34 @@
+import DoubleImages from "@/components/sections/double-img";
 import PageTitle from "@/components/sections/page-title";
+import Portfolio from "@/components/sections/portfolio";
+import { Button } from "@/components/ui/button";
+import { ArrowRightIcon } from "@radix-ui/react-icons";
 
 export default function Studios() {
-    return(
-        <PageTitle subtitle="studi" title="Tantissimi lavori fantastici e meravigliosi"/>
-    )
+  return (
+    <>
+      <PageTitle
+        subtitle="studi"
+        title="Qui sotto puoi trovare tantissimi progetti fantastici e meravigliosi"
+      />
+      <div className="bg-light text-light-foreground">
+        <section>
+          <div className="flex items-center justify-between">
+            <div className="flex flex-col gap-4">
+              <div className="uppercase text-muted-foreground text-sm">
+                in evidenza
+              </div>
+              <h1 className="font-medium">Equilibrio Corpo e Mente</h1>
+            </div>
+            <Button variant="link" size="zero">
+              Vai al sito
+              <ArrowRightIcon className="ml-3 h-4 w-4" />
+            </Button>
+          </div>
+        </section>
+      </div>
+      <DoubleImages />
+      <Portfolio cta={false}/>
+    </>
+  );
 }
---
+++ components/sections/double-img.tsx
@@ -0,0 +1,8 @@
+export default function DoubleImages() {
+  return (
+    <div className="flex">
+      <div className="h-[800px] w-1/2 bg-secondary" />
+      <div className="h-[800px] w-1/2 bg-card" />
+    </div>
+  );
+}
