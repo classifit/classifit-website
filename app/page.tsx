164278b267a54e9e82a6b8ce8dfbd8bdec65c3a0
Chiara Santi <santichiara@hotmail.com>
2023-10-27 09:47:39 +0200

    studios page and page title component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 import About from '@/components/sections/about'
 import Feature from '@/components/sections/feature'
-import Footer from '@/components/sections/footer'
 import Hero from '@/components/sections/hero'
-import Navbar from '@/components/sections/navbar'
 import Packages from '@/components/sections/packages'
 import Portfolio from '@/components/sections/portfolio'
 
---
+++ app/studi/page.tsx
@@ -0,0 +1,7 @@
+import PageTitle from "@/components/sections/page-title";
+
+export default function Studios() {
+    return(
+        <PageTitle subtitle="studi" title="Tantissimi lavori fantastici e meravigliosi"/>
+    )
+}
