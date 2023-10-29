f591261d1534f1f72b0f0d7a234322b171826674
Chiara Santi <santichiara@hotmail.com>
2023-10-29 11:39:24 +0100

    spin button animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -79,13 +79,13 @@ module.exports = {
         },
         "spin-button": {
           from: { transform: "rotate(0deg)" },
-          to: { transform: "rotate(260deg)" },
+          to: { transform: "rotate(360deg)" },
         },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
-        "spin-button": "spin-button 6s linear infinite"
+        "spin-button": "spin-button 8s linear infinite"
       },
     },
   },
