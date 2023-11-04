8cd0f94c81e98e3dc557a182cb95f408c032aeeb
Chiara Santi <santichiara@hotmail.com>
2023-11-04 10:39:52 +0100

    privacy and cookie policy

--- components/sections/footer.tsx
+++ components/sections/footer.tsx
@@ -8,9 +8,26 @@ export default function Footer() {
         <div className="lg:flex justify-between">
           <div>
             <div className="flex flex-col gap-6">
-              <Link className="menu-link" href="#" target="_blank">Privacy Policy</Link>
-              <Link className="menu-link" href="#" target="_blank">Cookie Policy</Link>
-              <Link className="menu-link" href="mailto:classifit.studio@gmail.com">classifit.studio@gmail.com</Link>
+              <Link
+                className="menu-link"
+                href="https://www.iubenda.com/privacy-policy/40888749"
+                target="_blank"
+              >
+                Privacy Policy
+              </Link>
+              <Link
+                className="menu-link"
+                href="https://www.iubenda.com/privacy-policy/40888749/cookie-policy"
+                target="_blank"
+              >
+                Cookie Policy
+              </Link>
+              <Link
+                className="menu-link"
+                href="mailto:classifit.studio@gmail.com"
+              >
+                classifit.studio@gmail.com
+              </Link>
             </div>
             <div className="py-6" />
             <Image
@@ -22,22 +39,46 @@ export default function Footer() {
           </div>
           <div className="flex flex-col gap-6 mt-16 lg:mt-0">
             <p className="text-sm">Servizi</p>
-            <Link className="menu-link" href="/servizi/prenotazioni">Prenotazioni</Link>
-            <Link className="menu-link" href="/servizi/calendario">Calendario lezioni</Link>
-            <Link className="menu-link" href="/servizi/pacchetti">Pacchetti a crediti</Link>
-            <Link className="menu-link" href="/servizi/sito-su-misura">Sito su misura</Link>
-            <Link className="menu-link" href="/servizi/servizi-aggiuntivi">Servizi aggiuntivi</Link>
+            <Link className="menu-link" href="/servizi/prenotazioni">
+              Prenotazioni
+            </Link>
+            <Link className="menu-link" href="/servizi/calendario">
+              Calendario lezioni
+            </Link>
+            <Link className="menu-link" href="/servizi/pacchetti">
+              Pacchetti a crediti
+            </Link>
+            <Link className="menu-link" href="/servizi/sito-su-misura">
+              Sito su misura
+            </Link>
+            <Link className="menu-link" href="/servizi/servizi-aggiuntivi">
+              Servizi aggiuntivi
+            </Link>
           </div>
           <div className="flex flex-col gap-6 mt-16 lg:mt-0">
             <p className="text-sm">Risorse</p>
-            <Link className="menu-link" href="/blog">Blog</Link>
-            <Link className="menu-link" href="/roadmap">Roadmap</Link>
+            <Link className="menu-link" href="/blog">
+              Blog
+            </Link>
+            <Link className="menu-link" href="/roadmap">
+              Roadmap
+            </Link>
           </div>
           <div className="flex flex-col gap-6 mt-16 lg:mt-0">
             <p className="text-sm">Classifit</p>
-            <Link className="menu-link" href="/studi">Studi</Link>
-            <Link className="menu-link" href="/aiuto">Aiuto</Link>
-            <Link className="menu-link" href="https://www.instagram.com/app.classi.fit/" target="_blank">Instagram</Link>
+            <Link className="menu-link" href="/studi">
+              Studi
+            </Link>
+            <Link className="menu-link" href="/aiuto">
+              Aiuto
+            </Link>
+            <Link
+              className="menu-link"
+              href="https://www.instagram.com/app.classi.fit/"
+              target="_blank"
+            >
+              Instagram
+            </Link>
           </div>
         </div>
       </section>
