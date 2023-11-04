fc7bab160b3b2e634b2e135a4f295136a31c3c53
Chiara Santi <santichiara@hotmail.com>
2023-11-04 16:56:49 +0100

    google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import ContactButton from "@/components/ui/contact-button";
 import Navbar from "@/components/sections/navbar";
 import Footer from "@/components/sections/footer";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,20 @@ export default function RootLayout({
         <ContactButton />
         {children}
         <Footer />
+        {/* Global site tag (gtag.js) - Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-K5BCRCHP85"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-K5BCRCHP85');
+        `}
+        </Script>
       </body>
     </html>
   );
