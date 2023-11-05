9a45a25e73ac652f29523daa78a0d02d35762654
Chiara Santi <santichiara@hotmail.com>
2023-11-05 16:24:27 +0100

    iubenda script fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
         `}
         </Script>
 
-        <Script type="text/javascript">
+        <Script id="iubenda" type="text/javascript">
           {`
           var _iub = _iub || [];
           _iub.csConfiguration = {"askConsentAtCookiePolicyUpdate":true,"cookiePolicyInOtherWindow":true,"floatingPreferencesButtonCaptionColor":"#FFFFFF00","floatingPreferencesButtonColor":"#FFFFFF","floatingPreferencesButtonDisplay":"anchored-bottom-right","perPurposeConsent":true,"siteId":3181733,"whitelabel":false,"cookiePolicyId":40888749,"lang":"it", "banner":{ "acceptButtonDisplay":true,"closeButtonRejects":true,"customizeButtonDisplay":true,"explicitWithdrawal":true,"listPurposes":true,"position":"float-bottom-right" }};
